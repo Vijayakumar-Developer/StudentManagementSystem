b1b15b5cf6fd16f8ea8e93617980713df0c9d81f
Vijayakumar-Developer <vijai1993@gmail.com>
2025-08-09 12:54:04 +0530

    first commit

---
+++ src/com/basic/sms/Student.java
@@ -0,0 +1,54 @@
+package com.basic.sms;
+
+public class Student {
+	private int id;
+	private String name;
+	private double gpa;
+	private String city;
+	private String University;
+	public Student(int id, String name, double gpa, String city, String university) {
+	//	super();
+		this.id = id;
+		this.name = name;
+		this.gpa = gpa;
+		this.city = city;
+		University = university;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getGpa() {
+		return gpa;
+	}
+	public void setGpa(double gpa) {
+		this.gpa = gpa;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getUniversity() {
+		return University;
+	}
+	public void setUniversity(String university) {
+		University = university;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
+
+}
